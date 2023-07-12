cc2108428e4ba595a8cfe8ece981150b102aba5b
Sourav Rai <souraviarrai@gmail.com>
2023-07-12 18:21:08 +0600

    part1

--- db/migrate/20230712114900_create_products.rb
+++ db/migrate/20230712114900_create_products.rb
@@ -2,6 +2,7 @@ class CreateProducts < ActiveRecord::Migration[7.0]
   def change
     create_table :products do |t|
       t.string :name
+      t.text :description
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,7 @@
 ActiveRecord::Schema[7.0].define(version: 2023_07_12_115314) do
   create_table "products", force: :cascade do |t|
     t.string "name"
+    t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
